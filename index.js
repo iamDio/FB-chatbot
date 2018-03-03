a6eecb5f64ae81064b12c92a0d5389763c05f5c7
iamDio <claudio.villani00@gmail.com>
2018-03-03 18:58:30 -0500

    added heroku deployment files

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('port', (process.env.PORT || 3000))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
-app.listen(3000, () => console.log('webhook server is listening on port 3000'))
+app.listen(app.get('port'), () => console.log('webhook server is listening on port 3000'))
